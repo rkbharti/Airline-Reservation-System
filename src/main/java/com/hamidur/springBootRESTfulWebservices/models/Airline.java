0ab0d4cabe68c31cec9dcbfb83576830cdf45f4b
ravi kumar bharti <ravikb2002@gmail.com>
2023-04-17 15:42:32 +0530

    initial

--- src/main/java/com/hamidur/springBootRESTfulWebservices/models/Airline.java
+++ src/main/java/com/hamidur/springBootRESTfulWebservices/models/Airline.java
@@ -16,24 +16,24 @@ import java.io.Serializable;
 import java.util.Objects;
 import java.util.Set;
 
-@Entity
-@Table(name = "airlines")
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Entity  //part of database table
+@Table(name = "airlines")       // to change or rename the table
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})     //store data in json format
 public class Airline implements Serializable
 {
-    @Id
+    @Id     // primary key of table
     @Column(name = "airline_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)   // by default name ke basis pe  but  for this data is form
     private Integer airlineId;
     @Column(name = "airline_name")
     private String airlineName;
-    @JsonIgnore
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "airline")
-    private Set<Airplane> airplanes;
+    @JsonIgnore  // to remove general errors
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "airline")    // relationship
+    private Set<Airplane> airplanes;   //genric data airplane  and set use to dont put duplicate values
 
     public Integer getAirlineId() {
         return airlineId;
-    }
+    }  //create method getairline and return airlineid i.e mention above in line 27
 
     public void setAirlineId(Integer airlineId) {
         this.airlineId = airlineId;
@@ -65,12 +65,12 @@ public class Airline implements Serializable
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode() {  //object class and is used to get a unique integer value, called the hash code
         return Objects.hash(getAirlineId(), getAirlineName());
     }
 
-    @Override
-    public String toString() {
+    @Override // method in a subclass is intended to override a method in its superclass.
+    public String toString() {   // convert an object to a string representation
         return "Airline{" +
                 "airlineId=" + airlineId +
                 ", airlineName='" + airlineName + '\'' +
